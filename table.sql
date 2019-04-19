e07ec160488b637cd6bf12ba16baf474e8b749b5
acoshift <acoshift@gmail.com>
2019-04-19 21:09:05 +0700

    update table

--- table.sql
+++ table.sql
@@ -19,7 +19,7 @@ create table pictures (
     tags       varchar[] not null default '{}',
     created_at timestamp not null default now(),
     primary key (id),
-    foreign key (user_id) references users
+    foreign key (user_id) references users on delete cascade
 );
 create index on pictures (created_at desc);
 create index on pictures (user_id, created_at desc);
@@ -29,6 +29,29 @@ create table favorites (
     picture_id bigint,
     created_at timestamp not null default now(),
     primary key (user_id, picture_id),
-    foreign key (user_id) references users (id),
-    foreign key (picture_id) references pictures (id)
+    foreign key (user_id) references users (id) on delete cascade,
+    foreign key (picture_id) references pictures (id) on delete cascade
 );
+
+create table comments (
+    id uuid,
+    picture_id bigint not null,
+    user_id uuid not null,
+    content varchar not null,
+    created_at timestamp not null default now(),
+    primary key (id),
+    foreign key (picture_id) references pictures (id) on delete cascade,
+    foreign key (user_id) references users (id) on delete cascade
+);
+create index on comments (picture_id, created_at desc);
+
+create table follows (
+    user_id uuid,
+    following_id uuid,
+    created_at timestamp not null default now(),
+    primary key (user_id, following_id),
+    foreign key (user_id) references users (id) on delete cascade,
+    foreign key (following_id) references users (id) on delete cascade
+);
+create index on follows (user_id, created_at desc);
+create index on follows (following_id, created_at desc);
